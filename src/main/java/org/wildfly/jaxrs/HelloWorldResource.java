405de443df34244b34434141283a7f8d96f57385
Muhammad Rosli <muhd7rosli@live.com>
2015-11-13 12:58:43 +0800

    Applying license to each file that is covered by Apache License, Version 2.0

--- src/main/java/org/wildfly/jaxrs/HelloWorldResource.java
+++ src/main/java/org/wildfly/jaxrs/HelloWorldResource.java
@@ -5,10 +5,24 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 /**
- * Created by Muhammad on 11/11/2015.
+ * Copyright 2015 Muhammad bin Rosli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Muhammad Rosli <muhd7rosli@live.com>
+ *
+ * This is a simple REST API class to respond greeting message.
  */
-
-// This class is located at "/helloworld"
 @Path("/helloworld")
 public class HelloWorldResource {
     // These method will process HTTP GET requests
